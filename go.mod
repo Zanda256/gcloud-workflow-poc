3254aa9bcc1d7bd9373e31fdb2adfb851e0be70f
Sekiranda Hamza <sekirandahamza@gmail.com>
2022-05-30 11:07:01 +0300

    poc draft

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Zanda256/gcloud-workflow-poc
+
+go 1.18
+
+require github.com/julienschmidt/httprouter v1.3.0
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/Zanda256/gcloud-workflow-poc/api/handlers"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := httprouter.New()
+	router.GET("/orders/:order/docs", handlers.GetDocStatus)
+	router.POST("/orders/:order/qc/callback", handlers.StoreCallback)
+	router.POST("/orders/:order/qc", handlers.UpdateQc)
+
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
